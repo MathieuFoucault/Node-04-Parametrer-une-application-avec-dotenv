ced18630f54a8f342fab01c47ecebc71401f67d7
MATHIEU FOUCAULT <foucault.mathieu.pro@gmail.com>
2024-10-12 14:33:44 +0200

    feat : index.js with packages

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+const myName = process.env.MY_NAME;
+const myCity = process.env.MY_CITY;
+const myLanguage = process.env.MY_LANGUAGE;
+
+console.log(`I am ${myName}, wilder in ${myCity}, and I love ${myLanguage}.`);
